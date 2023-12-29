9319eb380d995530820bb1328c7d3d625c9bb6ae
Marc Jeric Espiritu <mavvystudio@gmail.com>
2023-12-29 15:32:51 +0800

    fix: add Card variant

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,20 +1,68 @@
 import React, { forwardRef } from 'react';
+import { borderColors } from './css-classes';
 
 export type CardProps = {
   className?: string;
   children?: React.ReactNode;
   style?: any;
+  variant?: 'elevated' | 'filled' | 'outlined';
+  noPadding?: boolean;
+  onClick?: () => void;
 };
 
-const defaultCls =
-  'm3-card bg-surface-container-low shadow p-3 rounded-3xl overflow-hidden';
+const defaultCls = 'm3-card rounded-3xl overflow-hidden';
+const padding = 'py-[12px] px-[16px]';
+
+const getVariantCls = (variant: CardProps['variant']) => {
+  const elevatedCls = 'bg-surface-container-low shadow p-3';
+  const outlinedCls = `border ${borderColors['outline-variant']}`;
+
+  if (variant === 'elevated') {
+    return elevatedCls;
+  }
+  if (variant === 'outlined') {
+    return outlinedCls;
+  }
+  return '';
+};
+
+const actionCls = 'cursor-pointer';
+
+const createCls = ({
+  className,
+  variant,
+  noPadding,
+  clickable,
+}: {
+  className: string;
+  variant: CardProps['variant'];
+  noPadding?: boolean;
+  clickable: boolean;
+}) => {
+  const cls = getVariantCls(variant);
+  const p = noPadding ? '' : padding;
+  const a = clickable ? actionCls : '';
+
+  return `${defaultCls} ${className} ${cls} ${p} ${a}`;
+};
 
 export const Card = forwardRef<HTMLDivElement, CardProps>(
-  ({ className = '', ...props }, ref) => {
-    const cardCls = `${defaultCls} ${className}`;
+  ({ className = '', variant = 'elevated', ...props }, ref) => {
+    const clickable = Boolean(props.onClick);
+    const cardCls = createCls({
+      className,
+      variant,
+      noPadding: props.noPadding,
+      clickable,
+    });
 
     return (
-      <div ref={ref} style={props.style} className={cardCls}>
+      <div
+        ref={ref}
+        style={props.style}
+        className={cardCls}
+        onClick={props.onClick}
+      >
         {props.children}
       </div>
     );
